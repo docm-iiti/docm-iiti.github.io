66fb79ba193c7408f354be9dfe80a6f970ec21c6
Krishna Chaitanya <bkchaitan94@gmail.com>
2013-09-28 16:16:28 +0530

    Anglarified and added fb links

---
+++ js/controllers.js
@@ -0,0 +1,30 @@
+function teamCtrl($scope, $http){
+	$scope.teamMembers = [
+		{
+			"name": "Miroojin Bakshi",
+		    "designation": "B. Tech (CSE), II year",
+		    "contact": "9999999999",
+		    "fb": "https://www.facebook.com/mbakshi"
+		},{
+		    "name":"B Krishna Chaitanya",
+		    "designation": "B. Tech (CSE), II year",
+		    "contact": "8349462618",
+		    "fb": "https://www.facebook.com/The.BKC"
+		},{
+		    "name":"Atul Choudhary",
+		    "designation": "B. Tech (ME), II year",
+		    "contact": "9999999999",
+		    "fb": "https://www.facebook.com/atul.choudhary.904"
+		},{
+		    "name":"Shikhar Singh",
+		    "designation": "B. Tech (CSE), III year",
+		    "contact": "9999999999",
+		    "fb": "https://www.facebook.com/ShikharSingh007"
+		},{
+		    "name":"Aashish Aryan",
+		    "designation": "B. Tech (CSE), III year",
+		    "contact": "9999999999",
+		    "fb": "https://www.facebook.com/aashish.aaryan.3"
+		}
+	];
+}
