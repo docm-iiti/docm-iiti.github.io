fc89b18729d02a4f093d8af9400e6d28f18ab01d
Krishna Chaitanya <bkchaitan94@gmail.com>
2013-09-28 23:09:48 +0530

    Softcoded team members data

--- js/controllers.js
+++ js/controllers.js
@@ -1,30 +1,5 @@
 function teamCtrl($scope, $http){
-	$scope.teamMembers = [
-		{
-			"name": "Miroojin Bakshi",
-		    "designation": "B. Tech (CSE), II year",
-		    "email": "cse1200105@iiti.ac.in",
-		    "fb": "https://www.facebook.com/mbakshi"
-		},{
-		    "name":"B Krishna Chaitanya",
-		    "designation": "B. Tech (CSE), II year",
-		    "email": "ee1200206@iiti.ac.in",
-		    "fb": "https://www.facebook.com/The.BKC"
-		},{
-		    "name":"Atul Choudhary",
-		    "designation": "B. Tech (ME), II year",
-		    "email": "me1200308@iiti.ac.in",
-		    "fb": "https://www.facebook.com/atul.choudhary.904"
-		},{
-		    "name":"Shikhar Singh",
-		    "designation": "B. Tech (CSE), III year",
-		    "email": "cs1100132@iiti.ac.in",
-		    "fb": "https://www.facebook.com/ShikharSingh007"
-		},{
-		    "name":"Aashish Aryan",
-		    "designation": "B. Tech (CSE), III year",
-		    "email": "cs1100105@iiti.ac.in",
-		    "fb": "https://www.facebook.com/aashish.aaryan.3"
-		}
-	];
+	$http.get('data/team.json').success(function(data){
+		$scope.teamMembers = data;
+	});
 }
